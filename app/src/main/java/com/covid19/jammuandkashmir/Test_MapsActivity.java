431af787cd97555c50a744b76fc6a000cf2baa0a
anirudhbelwadi <belwadi.anirudh18@siesgst.ac.in>
2020-06-30 13:32:28 +0530

    Developed, without name and not released

---
+++ app/src/main/java/com/covid19/jammuandkashmir/Test_MapsActivity.java
@@ -0,0 +1,104 @@
+package com.covid19.jammuandkashmir;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Looper;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Test_MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    LatLng latLng;
+    private FusedLocationProviderClient fusedLocationClient;
+    private SupportMapFragment mapFragment;
+    LocationRequest mLocationRequest;
+    Location mLastLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test__maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(30000);
+        mLocationRequest.setFastestInterval(30000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        fusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+        mMap.setMyLocationEnabled(true);
+
+        // Test Center 1
+        final LatLng center1 = new LatLng(34.777226, 74.895860);
+        mMap.addMarker(new MarkerOptions().position(center1).title("Sher-e- Kashmir Institute of Medical Sciences, Srinagar").snippet("website: http://www.skims.ac.in/"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(center1));
+
+        //Test Center 2
+        final LatLng center2 = new LatLng(32.736338, 74.853888);
+        mMap.addMarker(new MarkerOptions().position(center2).title("Government Medical College, Jammu").snippet("website: http://www.gmcjammu.nic.in/"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(center2));
+        //  mMap.animateCamera(CameraUpdateFactory.zoomTo(6));
+
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                if(marker.getPosition().equals(center1)) {
+                    Uri uriUrl = Uri.parse("http://www.skims.ac.in/");
+                    Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                    startActivity(launchBrowser);
+                }
+                else if(marker.getPosition().equals(center2)) {
+                    Uri uriUrl = Uri.parse("http://www.gmcjammu.nic.in/");
+                    Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                    startActivity(launchBrowser);
+                }
+            }
+        });
+    }
+    LocationCallback mLocationCallback=new LocationCallback(){
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            for(Location location1 : locationResult.getLocations()) {
+                if (getApplicationContext() != null) {
+                    mLastLocation = location1;
+                    latLng = new LatLng(location1.getLatitude(), location1.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(7));
+                }
+            }
+
+        }
+    };
+    public void onBackPressed(){
+        stopService(new Intent(getApplicationContext(), Location_Services.class));
+        Intent intent = new Intent(Test_MapsActivity.this, home.class);
+        startActivity(intent);
+        finish();
+        return;
+
+    };
+}
